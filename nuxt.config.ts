53b0ab7aecc78c234599a398613dc0924a81ac0b
leadt63 <leadt63@gmail.com>
2025-04-18 10:44:58 -0700

    feat/auto-refresh-on-window-event

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,31 @@
 export default defineNuxtConfig({
- devtools: { enabled: false },
- ssr: false,
- srcDir: "src",
- css: ['~/assets/css/main.css'],
- routeRules: {
-	"/": { prerender: true }
-},
- modules: [
-	"@nuxt/ui",
-	'@pinia/nuxt'
-],
- compatibilityDate: "2025-04-10"
-});
+	devtools: { enabled: false },
+	ssr: false,
+	srcDir: "src",
+	css: ['~/assets/css/main.css'],
+	routeRules: {
+	   "/": { prerender: true }
+   },
+	modules: [
+	   "@nuxt/ui",
+	   '@pinia/nuxt'
+   ],
+	compatibilityDate: "2025-04-10",
+	vite: {
+	   optimizeDeps: {
+		 include: ['@tauri-apps/api']
+	   },
+	   build: {
+		 target: ['es2021', 'chrome100', 'safari13'],
+		 minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+		 sourcemap: !!process.env.TAURI_DEBUG,
+	   }
+	},
+	typescript: {
+	   tsConfig: {
+		 compilerOptions: {
+		   types: ['@tauri-apps/api']
+		 }
+	   }
+	}
+   });
