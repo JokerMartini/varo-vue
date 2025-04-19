920579c38725aeea13bf80a5b7872571f83b851d
Umair Malik <leadt63@gmail.com>
2025-04-19 14:43:57 -0700

    Revert "feat/auto-refresh-on-window-event"
    This reverts commit 53b0ab7aecc78c234599a398613dc0924a81ac0b.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,14 @@
 export default defineNuxtConfig({
-	devtools: { enabled: false },
-	ssr: false,
-	srcDir: "src",
-	css: ['~/assets/css/main.css'],
-	routeRules: {
-	   "/": { prerender: true }
-   },
-	modules: [
-	   "@nuxt/ui",
-	   '@pinia/nuxt'
-   ],
-	compatibilityDate: "2025-04-10",
-	vite: {
-	   optimizeDeps: {
-		 include: ['@tauri-apps/api']
-	   },
-	   build: {
-		 target: ['es2021', 'chrome100', 'safari13'],
-		 minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-		 sourcemap: !!process.env.TAURI_DEBUG,
-	   }
-	},
-	typescript: {
-	   tsConfig: {
-		 compilerOptions: {
-		   types: ['@tauri-apps/api']
-		 }
-	   }
-	}
-   });
+ devtools: { enabled: false },
+ ssr: false,
+ srcDir: "src",
+ css: ['~/assets/css/main.css'],
+ routeRules: {
+	"/": { prerender: true }
+},
+ modules: [
+	"@nuxt/ui",
+	'@pinia/nuxt'
+],
+ compatibilityDate: "2025-04-10"
+});
